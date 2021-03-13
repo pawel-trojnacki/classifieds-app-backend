0b428f5e8bad2efab58a47062f4448e5bf3b3f5b
Pawel Trojnacki <trojnacki.p@outlook.com>
2021-03-13 12:38:09 +0100

    Try with changed cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
-import * as helmet from 'helmet';
+// import * as helmet from 'helmet';
 import { config } from 'aws-sdk';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.CLIENT_URL, credentials: true });
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
